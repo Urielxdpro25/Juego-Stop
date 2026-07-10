14ace1f598d843c9c42b2ee4070211a42f42ea01
urielxdpro25 <urieluncos@gmail.com>
2026-07-10 01:27:12 -0300

    mejores en la ruleta y el input

--- service-worker.js
+++ service-worker.js
@@ -3,7 +3,7 @@
 // Versión del caché
 // ==========================================
 
-const CACHE = "gestionador-v1";
+const CACHE = "gestionador-v2";
 
 // Archivos que se guardarán para usar sin Internet
 const ARCHIVOS = [
